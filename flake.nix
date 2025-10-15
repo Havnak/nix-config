b100949520c5dfc4430e3734e00085fc5f402219
havnak <havardnakling@gmail.com>
2025-10-15 19:53:00 +0200

    flake: update 2025-10-15

--- flake.nix
+++ flake.nix
@@ -1,48 +1,47 @@
 {
-  description = "System-wide Ubuntu packages managed by a flake";
+    description = "System-wide Ubuntu packages managed by a flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
+    outputs = { self, nixpkgs }:
+        let
+        system = "x86_64-linux";
+    pkgs = import nixpkgs {
         inherit system;
-      };
+    };
 
-      # Python environment
-      systemPython = pkgs.python312.withPackages (ps: with ps; [
-        numpy
-	matplotlib
-      ]);
+# Python environment
+    systemPython = pkgs.python312.withPackages (ps: with ps; [
+            numpy
+            matplotlib
+    ]);
 
     in {
-      packages.${system}.default = pkgs.buildEnv {
-        name = "system-packages";
-        paths = with pkgs; [
-          git 
-          vim
-          curl
-          wget
-          lazygit
-          neovim
-          clang-tools
-          openssh
-          nmap
-          ripgrep
-          systemPython
-          alacritty
-          htop
-          tree
-          ncdu
-          uv
-          sioyek
-	  vlc
-	  vlc
-        ];
-      };
+        packages.${system}.default = pkgs.buildEnv {
+            name = "system-packages";
+            paths = with pkgs; [
+                git 
+                    vim
+                    curl
+                    wget
+                    lazygit
+                    neovim
+                    clang-tools
+                    openssh
+                    nmap
+                    ripgrep
+                    systemPython
+                    alacritty
+                    htop
+                    tree
+                    ncdu
+                    uv
+                    sioyek
+                    vlc
+            ];
+        };
     };
 }
 
