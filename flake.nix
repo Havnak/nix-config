f5f62714f14b85f958c048c3157d01d91da612bc
havnak <havardnakling@gmail.com>
2025-10-15 19:08:37 +0200

    nix flake for nix profile 2025.10.15

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "System-wide Ubuntu packages managed by a flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      # Python environment
+      systemPython = pkgs.python312.withPackages (ps: with ps; [
+        numpy
+	matplotlib
+      ]);
+
+    in {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "system-packages";
+        paths = with pkgs; [
+          git 
+          vim
+          curl
+          wget
+          lazygit
+          neovim
+          clang-tools
+          openssh
+          nmap
+          ripgrep
+          systemPython
+          alacritty
+          htop
+          tree
+          ncdu
+          uv
+          sioyek
+        ];
+      };
+    };
+}
+
