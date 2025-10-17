2cc11271c1178ebab79ad39e71023b151dc63fa1
havnak <havardnakling@gmail.com>
2025-10-17 13:45:28 +0200

    flake: update 2025-10-17

--- flake.nix
+++ flake.nix
@@ -1,54 +1,48 @@
 {
-    description = "System-wide Ubuntu packages managed by a flake";
+  description = "System-wide Ubuntu packages managed by a flake";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; };
 
-    outputs = { self, nixpkgs }:
-        let
-        system = "x86_64-linux";
-    pkgs = import nixpkgs {
-        inherit system;
-    };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
 
-# Python environment
-    systemPython = pkgs.python312.withPackages (ps: with ps; [
-            numpy
-            matplotlib
-    ]);
+      # Python environment
+      systemPython =
+        pkgs.python312.withPackages (ps: with ps; [ numpy matplotlib ]);
 
     in {
-        packages.${system}.default = pkgs.buildEnv {
-            name = "system-packages";
-            paths = with pkgs; [
-                    git 
-                    vim
-                    curl
-                    wget
-                    lazygit
-                    neovim
-                    clang-tools
-                    openssh
-                    nmap
-                    fd
-                    xsel
-                    ripgrep
-                    systemPython
-                    alacritty
-                    htop
-                    tree
-                    ncdu
-                    uv
-                    sioyek
-                    vlc
-                    gnumake
-                    gcc
-                    libgcc
-                    cargo
-                    rustc
-            ];
-        };
+      packages.${system}.default = pkgs.buildEnv {
+        name = "system-packages";
+        paths = with pkgs; [
+          git
+          vim
+          curl
+          wget
+          lazygit
+          neovim
+          clang-tools
+          openssh
+          nmap
+          fd
+          xsel
+          ripgrep
+          systemPython
+          htop
+          tree
+          ncdu
+          uv
+          sioyek
+          vlc
+          gnumake
+          gcc
+          libgcc
+          cargo
+          rustc
+          terminator
+        ];
+      };
     };
 }
 
